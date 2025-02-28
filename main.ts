f3121c5adb7a99a61c34983c19960b0e20d6a194
Fluffy0507 <martyna.techniek@gmail.com>
2025-02-28 14:36:37 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,64 @@
+let IR_waarde = 0
+IR.IR_init()
 let vooruit = 133
 let achteruit = 5
 let links = 69
 let rechts = 197
+let freeze = 205
 basic.forever(function () {
-    let IR_waarde = 0
-    basic.showNumber(IR_waarde)
+    if (IR_waarde == vooruit) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == achteruit) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == links) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == rechts) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == freeze) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    IR_waarde = IR.IR_read()
 })
